767b139c4aa1155dcf892f86c969b0cea9b66497
julien.marbach <julien.marbach@da2dd3ea-45b4-11de-b25d-9f9778916a3c>
2009-05-21 03:16:35 +0000

    pushing first working version

---
+++ _define.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('DC_RC_PATH')) { return; }
+ 
+$this->registerModule(
+	/* Name */			"Webcam widget",
+	/* Description*/		"Displays a regularly updated image",
+	/* Author */			"Julien MARBACH",
+	/* Version */			'1.0',
+	/* Permissions */		'admin'
+);
+?>
